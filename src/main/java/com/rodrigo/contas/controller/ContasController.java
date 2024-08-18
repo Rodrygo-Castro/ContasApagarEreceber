27da23d8717808e3b7086896415400bcb27eff08
Rodrigo Castro <109392058+Rodrygo-Castro@users.noreply.github.com>
2024-08-18 19:06:01 -0300

    implementando o metodo add para adicionar uma nova conta

--- src/main/java/com/rodrigo/contas/controller/ContasController.java
+++ src/main/java/com/rodrigo/contas/controller/ContasController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.rodrigo.contas.model.Contas;
 import com.rodrigo.contas.service.ContasService;
@@ -13,7 +15,7 @@ import com.rodrigo.contas.service.ContasService;
 @Controller
 @RequestMapping("/contas")
 public class ContasController {
-    
+
     @Autowired
     private ContasService contasService;
 
@@ -23,4 +25,12 @@ public class ContasController {
         model.addAttribute("listaContas", listaContas);
         return "index";//aqui vou retornar a view 
     }
+
+    @GetMapping("/adicionar")
+    public ModelAndView add(Contas contas) {
+        ModelAndView mv = new ModelAndView("adicionar"); // O nome da view deve corresponder ao arquivo HTML no diretório templates
+        mv.addObject("contas", contas);
+        return mv;
+    }
+
 }
